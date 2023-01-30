27d6bf59186fa7600ce876421a366b7dfea2110e
AndreiSanduta <andreisanduta@hotmail.com>
2023-01-30 22:05:28 +0000

    The spreed operator

--- arrow-functions.js
+++ arrow-functions.js
@@ -1,10 +1,19 @@
-/**
- * To run this file in Gitpod, use the 
- * command node arrow-functions.js in the terminal
- */
+// /**
+//  * To run this file in Gitpod, use the 
+//  * command node arrow-functions.js in the terminal
+//  */
 
-// Vanilla JavaScript Function
-// function addTwoNumbers (a, b) {
+// // Vanilla JavaScript Function
+// // function addTwoNumbers (a, b) {
+// //     //code block
+// //     return a + b;
+// // }
+
+// // let sum = addTwoNumbers (3, 5);
+
+// // console.log(sum)
+// // Arrow Function With Parameters
+// const addTwoNumbers = (a, b) => {
 //     //code block
 //     return a + b;
 // }
@@ -12,40 +21,59 @@
 // let sum = addTwoNumbers (3, 5);
 
 // console.log(sum)
-// Arrow Function With Parameters
-const addTwoNumbers = (a, b) => {
-    //code block
-    return a + b;
-}
 
-let sum = addTwoNumbers (3, 5);
+// // Single Line Arrow Function With Parameters
 
-console.log(sum)
+// const addTwoNumbers2 = (a, b) => a + b;
+// let sum2 = addTwoNumbers2(8, 9);
+// console.log(sum2);
 
-// Single Line Arrow Function With Parameters
+// // Implicit Returns
 
-const addTwoNumbers2 = (a, b) => a + b;
-let sum2 = addTwoNumbers2(8, 9);
-console.log(sum2);
+// const saySomething = message => console.log(message);
+// saySomething('Hello butchi butxhi!');
 
-// Implicit Returns
+// const sayHello = () => console.log('Hello');
+// sayHello();
 
-const saySomething = message => console.log(message);
-saySomething('Hello butchi butxhi!');
+// // Returning Multiple Lines
 
-const sayHello = () => console.log('Hello');
-sayHello();
-
-// Returning Multiple Lines
-
-const returnMultipleLines = () => (
-    `<p>
-        This is a multiple string!
-    </p>`
-)
-console.log(returnMultipleLines());
+// const returnMultipleLines = () => (
+//     `<p>
+//         This is a multiple string!
+//     </p>`
+// )
+// console.log(returnMultipleLines());
 
 
+//------------------lesson two-----------------
+/**
+ * To run this file in Gitpod, use the 
+ * command node the-spread-operator.js in the terminal
+ */
 
+// No spread operator
+let arr1 = [1, 2, 3];
+let arr2 = arr1;
+arr2.push(4);
+console.log("second array:", arr2);
+// Copying an array
 
+let arr3 = [4, 5, 6];
+let arr4 = [...arr3];
 
+arr4.push(7);
+console.log("third array:", arr3);
+console.log("fourth array:", arr4);
+
+// Copying an object
+let obj1 = {a: 1, b: 2, c: 3, };
+let obj2 = {...obj1, d:4 };
+let obj3 = {...obj1, b: 5 };
+console.log('first object', obj1);
+console.log('second object', obj2);
+console.log('third object', obj3);
+
+// Copying only part of an array/object
+let arr5 = [...arr1, {...obj1}, arr3, "x", "y", "z"];
+console.log(arr5);
